5be1da87194c57b3b3d3783b8adef336d6cbc9b7
anvarov <akmalanvaroff@gmail.com>
2018-03-07 20:20:44 -0600

    Added some styling and error handling function

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 window.onload = function() {
+
+    function errorHandler(error) {
+        console.log(error.message);    
+    }
+
     function success(response) {
         fetch('https://fcc-weather-api.glitch.me/api/current?lat=' + response.coords.latitude + '&lon=' + response.coords.longitude)
         .then(data => data.json())
         .then(jsonObject => {
             document.getElementById('cityName').innerText = jsonObject.name + ', ' + jsonObject.sys.country;
             document.getElementById('icon').setAttribute('src', jsonObject.weather[0].icon);
-            document.getElementsByTagName('i')[0].setAttribute('class', 'wi wi-wind from-' + jsonObject.wind.deg.toFixed() + '-deg');
-            document.getElementById('wind').innerText = windDirection(jsonObject.wind) + ', ' + jsonObject.wind.speed + ' knots';
+            document.getElementsByClassName('wind')[0].insertAdjacentHTML('afterbegin', '<h2 id=\'wind\'>' + windDirection(jsonObject.wind) + ', ' + jsonObject.wind.speed + ' knots ' + '</h2>');
+            document.getElementById('wind').insertAdjacentHTML('beforeend', '<i class=\'wi wi-wind from-' + jsonObject.wind.deg.toFixed() + '-deg\'></i>');
             document.getElementById('degree').innerText = jsonObject.main.temp.toFixed() + ' F';
+            document.getElementById('description').innerText = jsonObject.weather[0].main;
             console.log(jsonObject);
         })
     }
-    navigator.geolocation.getCurrentPosition(success)
+    navigator.geolocation.getCurrentPosition(success, errorHandler)
     function windDirection(input) {
         const direction = input.deg.toFixed();
         if (direction === 0 || direction === 360) {
